def7bad902146131c3dbc3de32167b462df67d90
Andrew Duthie <andrew@andrewduthie.com>
2021-07-18 07:39:05 -0400

    Switch to personal ESLint configuration

--- hijinks.js
+++ hijinks.js
@@ -1,43 +1,46 @@
-function hijinks( tag, attributes ) {
+function hijinks(tag, attributes) {
 	var isComponent = 'string' != typeof tag,
 		hasAttributes = null != attributes && attributes.constructor == Object,
-		children = [].slice.call( arguments, hasAttributes ? 2 : 1 ),
-		props, element, i, child;
+		children = [].slice.call(arguments, hasAttributes ? 2 : 1),
+		props,
+		element,
+		i,
+		child;
 
-	if ( isComponent ) {
+	if (isComponent) {
 		props = { children: children };
 	} else {
-		element = document.createElement( tag );
+		element = document.createElement(tag);
 	}
 
-	if ( hasAttributes ) {
-		for ( i in attributes ) {
-			if ( isComponent ) {
-				props[ i ] = attributes[ i ];
-			} else if ( i in element ) {
-				element[ i ] = attributes[ i ];
+	if (hasAttributes) {
+		for (i in attributes) {
+			if (isComponent) {
+				props[i] = attributes[i];
+			} else if (i in element) {
+				element[i] = attributes[i];
 			} else {
-				element.setAttribute( i, attributes[ i ] );
+				element.setAttribute(i, attributes[i]);
 			}
 		}
 	}
 
-	if ( isComponent ) {
-		return tag( props );
+	if (isComponent) {
+		return tag(props);
 	}
 
-	while ( children.length ) {
+	while (children.length) {
 		child = children.shift();
-		if ( null == child || child instanceof Array ) {
-			[].push.apply( children, child );
+		if (null == child || child instanceof Array) {
+			[].push.apply(children, child);
 			continue;
 		}
 
-		if ( ! child.nodeType ) {
-			child = document.createTextNode( '' + child );
+		if (!child.nodeType) {
+			child = document.createTextNode('' + child);
 		}
 
-		element.appendChild( child );
+		element.appendChild(child);
 	}
 
 	return element;
--- test/hijinks.js
+++ test/hijinks.js
@@ -1,151 +1,145 @@
-const expect = require( 'chai' ).expect;
-const h = require( '../' );
+const expect = require('chai').expect;
+const h = require('../');
 
-describe( 'hijinks()', () => {
-	before( () => {
+describe('hijinks()', () => {
+	before(() => {
 		global.HTMLElement = window.HTMLElement;
 		global.Node = window.Node;
-	} );
+	});
 
-	function Component( { className, children } ) {
-		return h( 'div', { className: className }, children );
+	function Component({ className, children }) {
+		return h('div', { className: className }, children);
 	}
 
 	const vary = {
 		arguments: {
 			arguments: [
-				[ { className: 'example', 'data-ok': 1 } ],
-				( element ) => {
-					expect( element.className ).to.equal( 'example' );
-					expect( element.getAttribute( 'data-ok' ) ).to.equal( '1' );
-				}
+				[{ className: 'example', 'data-ok': 1 }],
+				(element) => {
+					expect(element.className).to.equal('example');
+					expect(element.getAttribute('data-ok')).to.equal('1');
+				},
 			],
-			'undefined arguments': [
-				[ undefined ],
-				() => {}
-			],
-			'without arguments': [
-				[],
-				() => {}
-			]
+			'undefined arguments': [[undefined], () => {}],
+			'without arguments': [[], () => {}],
 		},
 		children: {
 			'no children': [
 				() => [],
-				( element ) => {
-					expect( element.childNodes ).to.be.empty;
-				}
+				(element) => {
+					expect(element.childNodes).to.be.empty;
+				},
 			],
 			'single child': [
-				( createChild ) => [ createChild() ],
-				( element ) => {
-					expect( element.childNodes ).to.have.lengthOf( 1 );
-				}
+				(createChild) => [createChild()],
+				(element) => {
+					expect(element.childNodes).to.have.lengthOf(1);
+				},
 			],
 			'array of children': [
-				( createChild ) => [ [ createChild(), createChild() ] ],
-				( element ) => {
-					expect( element.childNodes ).to.have.lengthOf( 2 );
-				}
+				(createChild) => [[createChild(), createChild()]],
+				(element) => {
+					expect(element.childNodes).to.have.lengthOf(2);
+				},
 			],
 			'variadic arguments children': [
-				( createChild ) => [ createChild(), createChild() ],
-				( element ) => {
-					expect( element.childNodes ).to.have.lengthOf( 2 );
-				}
-			]
+				(createChild) => [createChild(), createChild()],
+				(element) => {
+					expect(element.childNodes).to.have.lengthOf(2);
+				},
+			],
 		},
 		child: {
 			'null child': [
 				() => null,
-				( element ) => {
-					expect( element.childNodes ).to.be.empty;
+				(element) => {
+					expect(element.childNodes).to.be.empty;
 				},
-				false
+				false,
 			],
 			'undefined child': [
 				() => undefined,
-				( element ) => {
-					expect( element.childNodes ).to.be.empty;
+				(element) => {
+					expect(element.childNodes).to.be.empty;
 				},
-				false
+				false,
 			],
 			'zero child': [
 				() => 0,
-				( element ) => {
-					element.childNodes.forEach( ( childNode ) => {
-						expect( Node.TEXT_NODE === childNode.nodeType );
-						expect( '0' === childNode.nodeValue );
-					} );
+				(element) => {
+					element.childNodes.forEach((childNode) => {
+						expect(Node.TEXT_NODE === childNode.nodeType);
+						expect('0' === childNode.nodeValue);
+					});
 				},
-				true
+				true,
 			],
 			'empty string child': [
 				() => '',
-				( element ) => {
-					element.childNodes.forEach( ( childNode ) => {
-						expect( Node.TEXT_NODE === childNode.nodeType );
-						expect( '' === childNode.nodeValue );
-					} );
+				(element) => {
+					element.childNodes.forEach((childNode) => {
+						expect(Node.TEXT_NODE === childNode.nodeType);
+						expect('' === childNode.nodeValue);
+					});
 				},
-				true
+				true,
 			],
 			'string child': [
 				() => 'foo',
-				( element ) => {
-					element.childNodes.forEach( ( childNode ) => {
-						expect( Node.TEXT_NODE === childNode.nodeType );
-						expect( 'foo' === childNode.nodeValue );
-					} );
+				(element) => {
+					element.childNodes.forEach((childNode) => {
+						expect(Node.TEXT_NODE === childNode.nodeType);
+						expect('foo' === childNode.nodeValue);
+					});
 				},
-				true
+				true,
 			],
 			'element child': [
-				() => h( 'span' ),
-				( element ) => {
-					element.childNodes.forEach( ( childNode ) => {
-						expect( Node.ELEMENT_NODE === childNode.nodeType );
-						expect( 'SPAN' === childNode.nodeName );
-					} );
+				() => h('span'),
+				(element) => {
+					element.childNodes.forEach((childNode) => {
+						expect(Node.ELEMENT_NODE === childNode.nodeType);
+						expect('SPAN' === childNode.nodeName);
+					});
 				},
-				true
+				true,
 			],
 			'component child': [
-				() => h( Component, { className: 'example' }, 'Hello World' ),
-				( element ) => {
-					element.childNodes.forEach( ( childNode ) => {
-						expect( childNode.nodeName ).to.equal( 'DIV' );
-						expect( childNode.className ).to.equal( 'example' );
-						expect( childNode.textContent ).to.equal( 'Hello World' );
-					} );
+				() => h(Component, { className: 'example' }, 'Hello World'),
+				(element) => {
+					element.childNodes.forEach((childNode) => {
+						expect(childNode.nodeName).to.equal('DIV');
+						expect(childNode.className).to.equal('example');
+						expect(childNode.textContent).to.equal('Hello World');
+					});
 				},
-				true
-			]
-		}
+				true,
+			],
+		},
 	};
 
-	for ( const varyArguments in vary.arguments ) {
-		for ( const varyChildren in vary.children ) {
-			for ( const varyChild in vary.child ) {
-				it( [ varyArguments, varyChildren, varyChild ].join( ', ' ), () => {
-					const [ args, assertArgs ] = vary.arguments[ varyArguments ];
-					const [ createChildren, assertChildren ] = vary.children[ varyChildren ];
-					const [ createChild, assertChild, countChild ] = vary.child[ varyChild ];
-					const element = h( 'div', ...args, ...createChildren( createChild ) );
+	for (const varyArguments in vary.arguments) {
+		for (const varyChildren in vary.children) {
+			for (const varyChild in vary.child) {
+				it([varyArguments, varyChildren, varyChild].join(', '), () => {
+					const [args, assertArgs] = vary.arguments[varyArguments];
+					const [createChildren, assertChildren] = vary.children[varyChildren];
+					const [createChild, assertChild, countChild] = vary.child[varyChild];
+					const element = h('div', ...args, ...createChildren(createChild));
 
-					expect( element ).to.be.an.instanceOf( HTMLElement );
-					expect( element.nodeName ).to.equal( 'DIV' );
+					expect(element).to.be.an.instanceOf(HTMLElement);
+					expect(element.nodeName).to.equal('DIV');
 
-					assertArgs( element );
-					assertChild( element );
+					assertArgs(element);
+					assertChild(element);
 
-					if ( countChild ) {
-						assertChildren( element );
+					if (countChild) {
+						assertChildren(element);
 					} else {
-						expect( element.childNodes ).to.be.empty;
+						expect(element.childNodes).to.be.empty;
 					}
-				} );
+				});
 			}
 		}
 	}
-} );
+});
