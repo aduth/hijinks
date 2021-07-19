2624c409c8fcb618c2bfe28d628b5bb01d7cf616
Andrew Duthie <andrew@andrewduthie.com>
2021-07-18 21:23:58 -0400

    Add fragment and component types for JSX definitions

--- jsx-runtime.d.ts
+++ jsx-runtime.d.ts
@@ -1,5 +1,6 @@
 import { Attributes, Fragment } from './hijinks';
 
-export function jsx(type: string, props: Attributes): Node;
+export function jsx(tag: Fragment, props: Attributes): DocumentFragment;
+export function jsx(type: string | Component, props: Attributes): Node;
 
 export { jsx as jsxs, jsx as jsxDEV, Fragment };
